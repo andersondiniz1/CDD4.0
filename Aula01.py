7b1a94b8f178ce84220e06bce9ee3065829bfb1a
Anderson Celso <64502987+andersondiniz1@users.noreply.github.com>
2023-08-29 19:49:24 +0000

    git

--- Aula01.py
+++ Aula01.py
@@ -2,10 +2,25 @@
 
 Num = 3;
 if Num % 2 == 0:
-    print ("É par.");
+    print("É par.");
 else: 
     print("É impar.");
 
-############################### python /workspaces/CDD4.0/Aula01.py 
+############################### python /workspaces/CDD4.0/Aula01.py ###############################
+
+# inteiros = 10
+# reais = 10.0
+# char = 1 caracter, exemplo: 'L' (aspas simples)
+# string = 2 ou mais, exemplo: "Luana" (aspas duplas) | "1" + "2" = 12
+# boolranos = V or F | 1 or 0
+#
+# constates = numero fixo, não vai mudar durante execução do programa. exemplo PI = 3.14
+# variaveis = numero1 = 10, numero2 = 5 | podem ser alterados durante a execução
+
+############################### python /workspaces/CDD4.0/Aula01.py ###############################
+
+nome = "Anderson";
+print(nome);
+
 
 
