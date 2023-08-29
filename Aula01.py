5e0c8befd6f77f22d76788bf62c2caa96150106c
Anderson Celso <64502987+andersondiniz1@users.noreply.github.com>
2023-08-29 18:07:18 +0000

    git

--- Aula01.py
+++ Aula01.py
@@ -1,1 +1,11 @@
-# teste
+# Crie um algoritmo que leia um numero e diga se ele e par ou impar.
+
+Num = 3;
+if Num % 2 == 0:
+    print ("É par.");
+else: 
+    print("É impar.");
+
+###############################
+
+
