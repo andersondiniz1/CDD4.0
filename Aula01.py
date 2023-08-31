b38789fddd92fd8eabd3e54eeae3e69dfb194c14
Anderson Celso <64502987+andersondiniz1@users.noreply.github.com>
2023-08-31 18:13:44 +0000

    Aula01.1

--- Aula01.py
+++ Aula01.py
@@ -19,8 +19,42 @@ else:
 
 ############################### python /workspaces/CDD4.0/Aula01.py ###############################
 
-nome = "Anderson";
-print(nome);
+
+# # exibir alguma coisa na tela
+# nome = "Anderson"
+# print(nome)
+
+# #usuario digitar
+# nome = input("Digite seu nome: ")
+# print("Seu nome é: ", nome)
+
+# # Imput de 3 nomes e print  |  | 'f' para formatar texto
+# nome1 = input("Digite primeiro nome: ")
+# nome2 = input("Digite segundo nome: ")
+# nome3 = input("Digite terceiro nome: ")
+# print("Seus nomes são: ", '\n', nome1, '\n', nome2, '\n', nome3) # '\n' para colocar na linha abaixo
+# print(f" os 3 nomes são \n {nome1}, \n {nome2}, \n {nome3} ")    # {} para variavel dentro do texto| \n ou variveis em texto não precisa de virgula
+
+# # print numeros  e soma
+# numero1 = int(input("Digite o primeiro numero: ")) # int(input() para numeros inteiros, é conseguir somar.
+# numero2 = int(input("Digite o segundo numero: "))
+# print(f"Numeros: \n {numero1}, {numero2}")
+# soma = numero1 + numero2
+# print(f"Soma dos numeros é: \n {soma}")
+
+# # Media de 2 notas
+# nome = input("Digite seu nome: ")
+# nota1 = int(input("Digite primeira nota: "))
+# nota2 = int(input("Digite segunda nota: "))
+# soma = nota1 + nota2
+# media = soma / 2
+# # media = (nota1 + nota2) / 2
+# print(f"Seu nome é: {nome}. \nÉ sua média é: {media}")
+
+
+
+
+
 
 
 
