86ed5641fe2afd08e8c8ef27a00650346f415956
Anderson Celso <64502987+andersondiniz1@users.noreply.github.com>
2023-09-05 18:57:20 +0000

    Aula02.2

--- Aula02.py
+++ Aula02.py
@@ -16,24 +16,47 @@
 # nota2 = float(input("Digite a segunda nota: "))
 # nota3 = float(input("Digite a terceira nota: "))
 
-# # Calculo da media
-# media = ((nota1 + nota2) + nota3) / 3
+# Calculo da media
+## media = ((nota1 + nota2) + nota3) / 3
 
-# # Condição sé passou ou não
-# # if media >= 7 and :
-# #     print(f"- Voce passou, sua média é: {media:.2f}")    # :2f para não dar numero decimal enorme
-# # else:
-# #     print(f"- Voce reprovou, sua média é: {media:.2f}")  # :2f para não dar numero decimal enorme
+## Condição sé passou ou não
+## if media >= 7 and :
+#     print(f"- Voce passou, sua média é: {media:.2f}")    # :2f para não dar numero decimal enorme
+# else:
+#     print(f"- Voce reprovou, sua média é: {media:.2f}")  # :2f para não dar numero decimal enorme
+
+############################### ou ###############################
 
 # # Condição sé passou ou não
 # # :2f para não dar numero decimal enorme
-# if media < 7:
-#     if media < 4:
-#         print(f"- Voce reprovou, sua média é: {media:.2f}")
+# if nota1 < 11 or nota2 < 11 or nota3 < 11 or nota1 > 0 or nota2 > 0 or nota3 > 0:
+#     media = ((nota1 + nota2) + nota3) / 3
+#     if media < 7:
+#         if media < 4:
+#             print(f"- Voce reprovou, sua média é: {media:.2f}")
+#         else:
+#             print(f"- Voce ficou de recuperação, sua média é: {media:.2f}")
 #     else:
-#         print(f"- Voce ficou de recuperação, sua média é: {media:.2f}")
+#         print(f"- Voce passou, sua média é: {media:.2f}")
 # else:
-#     print(f"- Voce passou, sua média é: {media:.2f}")
+#     print("Notas invalidas!")
+
+############################### ou ###############################
+
+
+# invalido = nota1 or nota2 or nota3 > 10 or nota1 or nota2 or nota3 < 0
+# if invalido:
+#     print("Notas invalidas!")
+# else:
+#     media = ((nota1 + nota2) + nota3) / 3
+#     if media < 7:
+#         if media < 4:
+#             print(f"- Voce reprovou, sua média é: {media:.2f}")
+#         else:
+#             print(f"- Voce ficou de recuperação, sua média é: {media:.2f}")
+#     else:
+#        print(f"- Voce passou, sua média é: {media:.2f}")
+
 
 ############################### python /workspaces/CDD4.0/Aula01.py ###############################
 
