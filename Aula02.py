24eb7951ad5d52e9a8a3f782273d6610ca2d5bec
Anderson Celso <64502987+andersondiniz1@users.noreply.github.com>
2023-09-05 18:09:12 +0000

    Aula02.1 git push

---
+++ Aula02.py
@@ -0,0 +1,39 @@
+# # Variaveis
+# n1 = int(input("1º Numero: "))
+# n2 = int(input("2º Numero: "))
+
+# # Estrutura logica
+# if n1 > n2:
+#     print(f"- Ordem crescente: {n2}, {n1}.")
+# else:
+#     print(f"- Ordem crescente: {n1}, {n2}.")
+
+############################### python /workspaces/CDD4.0/Aula01.py ###############################
+
+
+# # Variaveis notas
+# nota1 = float(input("Digite a promeira nota: "))
+# nota2 = float(input("Digite a segunda nota: "))
+# nota3 = float(input("Digite a terceira nota: "))
+
+# # Calculo da media
+# media = ((nota1 + nota2) + nota3) / 3
+
+# # Condição sé passou ou não
+# # if media >= 7 and :
+# #     print(f"- Voce passou, sua média é: {media:.2f}")    # :2f para não dar numero decimal enorme
+# # else:
+# #     print(f"- Voce reprovou, sua média é: {media:.2f}")  # :2f para não dar numero decimal enorme
+
+# # Condição sé passou ou não
+# # :2f para não dar numero decimal enorme
+# if media < 7:
+#     if media < 4:
+#         print(f"- Voce reprovou, sua média é: {media:.2f}")
+#     else:
+#         print(f"- Voce ficou de recuperação, sua média é: {media:.2f}")
+# else:
+#     print(f"- Voce passou, sua média é: {media:.2f}")
+
+############################### python /workspaces/CDD4.0/Aula01.py ###############################
+
