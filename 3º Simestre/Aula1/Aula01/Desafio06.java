a53d19b9113cc633d5af23233f6cd085b171c799
Anderson Celso <64502987+andersondiniz1@users.noreply.github.com>
2023-11-26 01:08:09 +0000

    java5

--- 3º Simestre/Aula1/Aula01/Desafio06.java
+++ 3º Simestre/Aula1/Aula01/Desafio06.java
@@ -4,35 +4,48 @@ import java.util.Scanner;
 
 public class Desafio06 {
     
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        int count = 0;
 
-        System.out.println("Telefonou para a vítima? /n Digite 1 para sim, 2 para não: "); 
+        System.out.println("Telefonou para a vítima? Digite 1 para sim, 2 para não: "); 
         int resp1 = entrada.nextInt();
+        if(resp1 == 1){
+            count++;
+        }
 
-        System.out.println("Esteve no local do crime? /n Digite 1 para sim, 2 para não: "); 
+        System.out.println("Esteve no local do crime? Digite 1 para sim, 2 para não: "); 
         int resp2 = entrada.nextInt();
+        if(resp2 == 1){
+            count++;
+        }
 
-        System.out.println("Mora perto da vítima? /n Digite 1 para sim, 2 para não: "); 
+        System.out.println("Mora perto da vítima? Digite 1 para sim, 2 para não: "); 
         int resp3 = entrada.nextInt();
+        if(resp3 == 1){
+            count++;
+        }
             
-        System.out.println("Devia para a vítima? /n Digite 1 para sim, 2 para não: "); 
+        System.out.println("Devia para a vítima? Digite 1 para sim, 2 para não: "); 
         int resp4 = entrada.nextInt();
+        if(resp4 == 1){
+            count++;
+        }
 
         System.out.println("Já trabalhou com a vítima? /n Digite 1 para sim, 2 para não: "); 
-        int resp5 = entrada.nextInt();     
+        int resp5 = entrada.nextInt();
+        if(resp5 == 1){
+            count++;
+        }     
 
-        int resultado = resp1 + resp2 + resp3 + resp4 + resp5;
-    
-        if (resultado == 8){
-            System.out.println("Suspeito"); 
-        } else if (resultado == 7 && resultado == 6){
+        if (count == 5){
+            System.out.println("Assassino"); 
+        } else if (count == 4 || count == 3){
             System.out.println("Cúmplice"); 
-        } else if (resultado == 5){
-            System.out.println("Assasino"); 
+        } else if (count == 2){
+            System.out.println("Suspeito"); 
         } else {
             System.out.println("Inocente"); 
         }
     }
-
 }
