6a8a8b595ed589ab911820839327b9f33242de73
Anderson Celso <64502987+andersondiniz1@users.noreply.github.com>
2023-12-06 20:04:06 +0000

    Modificadores

---
+++ 3º Simestre/Aula1/Aula01/Aula26.java
@@ -0,0 +1,6 @@
+// puclic = Liberado
+// private = não podem ser acessados ou usados em outra classe.
+// protected = pode ser acessado atraves de herança ou classes do mesmo pactoe.
+// default = somente no mesmo pacote/packege, não se declara
+// final = final in idade = 22 | final transforma a varial em constante
+// abstract = classe abstract não gera objeto, para definir escopo do filho, so serva para herança
