4c10a27f24c7ef9eb19d6780b5080fa6961d779f
Anderson Celso <64502987+andersondiniz1@users.noreply.github.com>
2023-12-14 19:56:42 +0000

    Encapsulamento

---
+++ Encapsulamento/funcionarios.java
@@ -0,0 +1,14 @@
+package Encapsulamento;
+
+public class funcionarios {
+ 
+    public String nome;
+    public String endereco;
+    public String cpf;
+    public String rg;
+    public String telefone;
+    public String email;
+    public Double salario;
+    public int q_filhos;
+
+}
---
+++ Encapsulamento/recursohumanos.java
@@ -0,0 +1,16 @@
+package Encapsulamento;
+
+public class recursohumanos {
+ 
+    public static void main(String[] args) {
+        funcionarios i = new funcionarios();
+        i.nome = "Marcelo da Silva";
+        i.endereco = "Rua da cacha prego";
+        i.cpf = "123.123.123.33";
+        i.rg = "321.321.32";
+        i.telefone = "81 91234-4321";
+        i.email = "Marcelo@gmail.com";
+        i.salario = 10.000;
+        i.q_filhos = 2;
+    }
+}
